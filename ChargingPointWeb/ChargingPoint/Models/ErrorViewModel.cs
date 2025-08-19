092856a234b820b440e391cfb94be3066e211d8e
BaoTran <117933868+BaoTran09@users.noreply.github.com>
2025-08-19 22:35:30 +0700

    Remove old ChargingPoint folder

--- ChargingPointWeb/ChargingPoint/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChargingPoint.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
