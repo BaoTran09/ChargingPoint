8f4fce02c68db81c44814eaf6f1496e0587f5843
BaoTran <117933868+BaoTran09@users.noreply.github.com>
2025-08-19 22:39:29 +0700

    Upload ChargingPoint project

---
+++ ChargingPointWeb/ChargingPoint/Controllers/StationController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChargingPoint.DB;
+
+namespace ChargingPoint.Controllers
+{
+    public class StationController : Controller
+    {
+        private readonly StoreDBContext _context;
+
+        public StationController(StoreDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Station.ToListAsync());
+        }
+
+        // GET: Stores/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Station
+                .FirstOrDefaultAsync(m => m.StationId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // GET: Stores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StationId,Tag,Name,StationType,Address,Latitude,Longitude,Notes,CreatedAt,BuiltDate")] Station store)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(store);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(store);
+        }
+
+        // GET: Stores/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Station.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+            return View(store);
+        }
+
+        // POST: Stores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("StationId,Tag,Name,StationType,Address,Latitude,Longitude,Notes,CreatedAt,BuiltDate")] Station store)
+        {
+            if (id != store.StationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(store);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StoreExists(store.StationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(store);
+        }
+
+        // GET: Stores/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Station
+                .FirstOrDefaultAsync(m => m.StationId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // POST: Stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var store = await _context.Station.FindAsync(id);
+            if (store != null)
+            {
+                _context.Station.Remove(store);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StoreExists(long id)
+        {
+            return _context.Station.Any(e => e.StationId == id);
+        }
+    }
+}
